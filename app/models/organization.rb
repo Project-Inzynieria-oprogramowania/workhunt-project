920b7139ad1b1a64a2344a3bf87bb9ddfc7721e8
Andrej <162591604+teqoshi@users.noreply.github.com>
2024-04-04 12:09:53 +0200

    Update validates

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,22 @@
 class Organization < ApplicationRecord
+    PASSWORD_FORMAT = /\A
+        (?=.{8,})          # Must contain 8 or more characters
+        (?=.*\d)           # Must contain a digit
+        (?=.*[a-z])        # Must contain a lower case character
+        (?=.*[A-Z])        # Must contain an upper case character
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+
     has_secure_password
     
     validates :login, presence: true, uniqueness: true
+    validates :password, format: { with: PASSWORD_FORMAT }
     validates :password_digest, presence: true
     validates :name, presence: true
+
+    before_save :downcase_login
+    
+    def downcase_login
+        login.downcase!
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
+    PASSWORD_FORMAT = /\A
+        (?=.{8,})          # Must contain 8 or more characters
+        (?=.*\d)           # Must contain a digit
+        (?=.*[a-z])        # Must contain a lower case character
+        (?=.*[A-Z])        # Must contain an upper case character
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+
     has_secure_password
-    
+
     validates :login, presence: true, uniqueness: true
+    validates :password, format: { with: PASSWORD_FORMAT }
     validates :password_digest, presence: true
     validates :name, presence: true
     validates :surname, presence: true
 
+    before_save :downcase_login
+    
+    def downcase_login
+        login.downcase!
+    end
     # правила обработки
     #   validates [имя поля], [проверки]
     # Проверки:
