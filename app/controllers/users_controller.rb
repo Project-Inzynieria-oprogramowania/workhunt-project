2a0c31bfde85aa811755be2c4db101b9ecd95f92
KristoMatsumoto <118841278+KristoMatsumoto@users.noreply.github.com>
2024-04-07 01:00:40 +0200

    Add login-pass editation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :check_no_authenticate, only: [:new, :create]
+    before_action :check_authenticate, only: [:edit, :update]
 
     def new
         session[:current_time] = Time.now
@@ -7,7 +8,6 @@ class UsersController < ApplicationController
     end
 
     def create
-        puts "==============\n#{params.inspect}\n==============="
         @user = User.new user_params
         if @user.save
             sign_in(@user)
@@ -18,15 +18,54 @@ class UsersController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
-    # def index
-    # end
-    # def show
-    #     #@user = User.find(params[:id])
-    # end
+    
+    def edit
+        # puts "=============3\n#{@user.inspect}\n=============="
+        @user ||= User.find_by id: session[:user_id]
+    end
+
+    def update
+        @user = User.find_by id: session[:user_id]
+        current_params = user_params
+
+        # Сheck password only when specifying a new one
+        if user_params[:password].blank?
+            current_params = user_params.except(:password, :password_confirmation, :old_password)
+        end
+
+        if @user.update(current_params)
+            # case @user.account_type
+            # when 'person'
+            #     @person = @user.account || @user.build_account(type: 'Person')
+            #     @person.update(person_params)
+            # when 'organization'
+            #     @organization = @user.account || @user.build_account(type: 'Organization')
+            #     @organization.update(organization_params)
+            # end
+            # flash[:success] = "User profile updated successfully"
+            redirect_to user_settings_path
+        else
+            # @user = User.new user_params
+            # @user.build_person if @user.person?.nil?
+            render :edit, status: :unprocessable_entity
+        end
+    end
 
     private
 
     def user_params
-        params.require(:user).permit(:login, :password, :password_confirmation, :account_type)
+        params.require(:user).permit(:login, :password, :password_confirmation, :account_type, :old_password)
     end
+
+    # def user_params_all
+    #     params.require(:user).permit(:id, :login, :password, :password_confirmation, :old_password, person_attributes: [:name, :surname, :sex, :DOB, :about])
+    # end
+
+    # def person_params
+    #     params.require(:person).permit(:name, :surname, :sex, :DOB, :about)
+    # end
+
+    # def organization_params
+    #     params.require(:organization).permit(:name, :about)
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root :to => "home#index"
 
+  # Registration
   get '/registration', to: 'users#new'
   post '/users', to: 'users#create'
-  # get '/login', to: 'users#login'
 
+  # Log In - Out
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  #get '/users/:id', to: 'users#show'#, as: 'user'
-  # resources :user, only: [:show]
+  # Update User information
+  get '/user/settings', to: 'users#edit'
+  patch '/user/update', to: 'users#update'
+
+  # get '/user/:id', to: 'users#show'#, as: 'user'
 end
