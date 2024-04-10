ca364966182b051792de13e83f38a6b576656ccf
Andrej <162591604+teqoshi@users.noreply.github.com>
2024-04-10 03:02:52 +0200

    Constants for vacancy

---
+++ app/assets/config/initializers/work_constants.rb
@@ -0,0 +1,56 @@
+JOB_TYPES = {
+  'IT' => ['Software Developer', 'Software Testing Engineer', 'System Administrator', 'Data Analyst', 'UX/UI Designer', 'Information Security Specialist'],
+  'Logistics' => ['Logistics Manager', 'Procurement Specialist', 'Warehouse Accountant', 'Logistics Operations Coordinator', 'Customs Declaration Specialist'],
+  'Business' => ['Sales Manager', 'Marketing Analyst', 'Financial Analyst', 'Product Manager', 'HR Manager', 'Business Analyst'],
+  'Medicine' => ['Doctor', 'Nurse', 'Pharmacist', 'Dentist', 'Medical Laboratory Technician'],
+  'Education' => ['Teacher', 'University Lecturer', 'Trainer', 'Pedagogical Psychologist', 'Education Consultant']
+  # and ect...
+}
+
+WORKING_TIME = [
+  'Full-time',        # Полная занятость
+  'Part-time',        # Частичная занятость
+  'Contract',         # Контракт
+  'Temporary',        # Временная работа
+  'Internship',       # Стажировка
+  'Additional work'   # Дополнительная работа
+]
+
+EDUCATION = [
+  'None',                   # Нет требований по образованию
+  'High School Diploma',    # Среднее образование
+  'Associate Degree',       # Диплом колледжа
+  "Bachelor's Degree",      # Бакалавр
+  "Master's Degree",        # Магистр
+  'PhD'                     # Доктор наук
+]
+
+SUBORDINATION_LEVEL = [
+  'No Subordinates',    # Нет подчиненных
+  'Junior',             # Младший сотрудник
+  'Middle',             # Средний сотрудник
+  'Senior',             # Старший сотрудник
+  'Manager',            # Менеджер
+  'Executive'           # Руководитель
+]
+
+CONTRACT_TYPE = [
+  'Intern/Trainee',     # Интерн или стажер
+  'Assistant',          # Помощник
+  'Junior Specialist',  # Начинающий специалист (Джуниор)
+  'Middle Specialist',  # Специалист среднего уровня
+  'Senior Specialist',  # Опытный специалист (Сеньор)
+  'Manager',            # Руководитель
+  'Supervisor',         # Менеджер
+  'Director',           # Директор
+  'President',          # Президент
+  'Laborer'             # Работник физического труда
+]
+
+STATUS = [
+  'Opened',     # Открыта
+  'Closed',     # Закрыта
+  'Active',     # Ведется отбор кандидатов
+  'Pending',    # В ожидании публикации \ открытия
+  'Withdrawn'   # Черновик?
+]
