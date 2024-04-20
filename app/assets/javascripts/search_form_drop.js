7dde76c481d50bde5a8f68743b4c68bfd6ccf365
markver <vereshchaginmark2022@gmail.com>
2024-04-20 17:56:45 +0200

    Search page, vacancy pages and small changes
    Added search page and vacancy creation and edition pages. Made slight changes to the search page and updated some css file names. Implemented some small code changes.

--- app/assets/javascripts/search_form_drop.js
+++ app/assets/javascripts/search_form_drop.js
@@ -1,4 +1,5 @@
 var drop_form = document.getElementById('search-drop-block')
+var drop_button = document.getElementById('search-form-default')
 document.getElementById('search-form-default').addEventListener('click', function(event) {
     if (event.target.id !== 'search-form-default') {
         return
@@ -6,7 +7,13 @@ document.getElementById('search-form-default').addEventListener('click', functio
 
     if (drop_form.style.display == 'none'){
         drop_form.style.display = ''
+        drop_button.style.backgroundColor = '#844da0'
+        drop_button.style.borderRadius = '0px 0px 0px 0px'
+        drop_button.style.color = '#e1c1f1'
     } else {
         drop_form.style.display = 'none'
+        drop_button.style.backgroundColor = '#663C7B'
+        drop_button.style.borderRadius = '0px 0px 10px 10px'
+        drop_button.style.color = '#E0ACFA'
     }
 })
