6e1251255970a508ce99d34f14a3bfdc05e09a6b
Andrej <162591604+teqoshi@users.noreply.github.com>
2024-04-20 20:16:55 +0200

    Update validates

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,5 +2,5 @@ class Email < ApplicationRecord
     belongs_to :user
 
     validates :address, presence: true
-    validates :address, :with => /@/, if: -> { address.present? }
+    validates :address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email" }, if: -> { address.present? }
 end
