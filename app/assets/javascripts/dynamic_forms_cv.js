db45b0b3743eccb9d723f8c1afbd9137c325d876
KristoMatsumoto <118841278+KristoMatsumoto@users.noreply.github.com>
2024-04-25 16:13:00 +0200

    Lesson corrections
    Co-Authored-By: markver <113990877+Markvereshch@users.noreply.github.com>
    Co-Authored-By: Andrei <162591604+teqoshi@users.noreply.github.com>

--- app/assets/javascripts/dynamic_forms_cv.js
+++ app/assets/javascripts/dynamic_forms_cv.js
@@ -4,41 +4,44 @@ var addExperienceButton = document.querySelector('#add-experience');
 var addLanguageButton = document.querySelector('#add-language');
 
 addEducationButton.addEventListener('click', function() {
-    var index = document.getElementsByClassName('education-form').length;
+    var index = document.getElementsByClassName('education-form').length
     fetch(`/cv/education/add?index=${index}`)
         .then(response => response.text())
         .then(html => {
-            const formsContainer = addEducationButton.closest('form');
-            const newForm = document.createElement('div');
-            newForm.innerHTML = html;
+            const formsContainer = addEducationButton.closest('div')
+            const newForm = document.createElement('div')
+            newForm.classList += 'education-form-block'
+            newForm.innerHTML = html
 
-            formsContainer.insertBefore(newForm, addEducationButton);
+            formsContainer.insertBefore(newForm, addEducationButton)
     });
 });
 
 addExperienceButton.addEventListener('click', function() {
-    var index = document.getElementsByClassName('experience-form').length;
+    var index = document.getElementsByClassName('experience-form').length
     fetch(`/cv/experience/add?index=${index}`)
         .then(response => response.text())
         .then(html => {
-            const formsContainer = addExperienceButton.closest('form');
-            const newForm = document.createElement('div');
-            newForm.innerHTML = html;
+            const formsContainer = addExperienceButton.closest('div')
+            const newForm = document.createElement('div')
+            newForm.classList += 'experience-form-block'
+            newForm.innerHTML = html
 
-            formsContainer.insertBefore(newForm, addExperienceButton);
+            formsContainer.insertBefore(newForm, addExperienceButton)
     });
 });
 
 addLanguageButton.addEventListener('click', function() {
-    var index = document.getElementsByClassName('language-form').length;
+    var index = document.getElementsByClassName('language-form').length
     fetch(`/cv/language/add?index=${index}`)
         .then(response => response.text())
         .then(html => {
-            const formsContainer = addLanguageButton.closest('form');
-            const newForm = document.createElement('div');
-            newForm.innerHTML = html;
+            const formsContainer = addLanguageButton.closest('div')
+            const newForm = document.createElement('div')
+            newForm.classList += 'language-form-block'
+            newForm.innerHTML = html
 
-            formsContainer.insertBefore(newForm, addLanguageButton);
+            formsContainer.insertBefore(newForm, addLanguageButton)
     });
 });
 
@@ -50,24 +53,24 @@ document.addEventListener('click', function(event) {
         fetch(url, {method: 'DELETE'})
             .then(response => response.text())
             .then(html => {
-                const formToRemove = event.target.closest('.education-form');
-                formToRemove.remove();
+                const formToRemove = event.target.closest('div.education-form-block')
+                formToRemove.remove()
         });
     } else if (event.target.classList.contains('remove-experience')) {
         var url = indetificator ? `/cv/experience/remove?id=${indetificator}` : `/cv/experience/remove`
         fetch(url, {method: 'DELETE'})
             .then(response => response.text())
             .then(html => {
-                const formToRemove = event.target.closest('.experience-form');
-                formToRemove.remove();
+                const formToRemove = event.target.closest('div.experience-form-block')
+                formToRemove.remove()
         });
     } else if (event.target.classList.contains('remove-language')) {
         var url = indetificator ? `/cv/language/remove?id=${indetificator}` : `/cv/language/remove`
         fetch(url, {method: 'DELETE'})
             .then(response => response.text())
             .then(html => {
-                const formToRemove = event.target.closest('.language-form');
-                formToRemove.remove();
+                const formToRemove = event.target.closest('div.language-form-block')
+                formToRemove.remove()
         });
     }
 });
