3915a09f65e365dfec6de263449eaf921c38de4f
markver <vereshchaginmark2022@gmail.com>
2024-05-05 13:20:13 +0200

    CV search results page and minor changes
    -If a user don't have contact informations (any type) it won't be shown on the profile page.
    -The vacancy.css file is now called brief_card.css
    -The user can switch between the CV search page and the Vacancy search page.

--- app/assets/javascripts/search_form_drop.js
+++ app/assets/javascripts/search_form_drop.js
@@ -1,5 +1,6 @@
 var drop_form = document.getElementById('search-drop-block')
 var drop_button = document.getElementById('search-form-default')
+var arrows = document.querySelector('#search-form-default img')
 document.getElementById('search-form-default').addEventListener('click', function(event) {
     if (event.target.id !== 'search-form-default') {
         return
@@ -10,10 +11,12 @@ document.getElementById('search-form-default').addEventListener('click', functio
         drop_button.style.backgroundColor = '#844da0'
         drop_button.style.borderRadius = '0px 0px 0px 0px'
         drop_button.style.color = '#e1c1f1'
+        arrows.style.transform = 'rotate(180deg)'
     } else {
         drop_form.style.display = 'none'
         drop_button.style.backgroundColor = '#663C7B'
         drop_button.style.borderRadius = '0px 0px 10px 10px'
         drop_button.style.color = '#E0ACFA'
+        arrows.style.transform = ''
     }
 })
