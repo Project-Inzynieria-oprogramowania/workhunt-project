f7b91502426738c8b3a1caaca4d7f0e77dffa44d
markver <vereshchaginmark2022@gmail.com>
2024-05-09 13:41:10 +0200

    CV rendering (part 2) and minor changes
    -Now languages and experiences are rendered on the pdf page. Default font-size has been changed to 10px.
    -Now the name and surname on the Show CV page are on the same line.
    -Switch-buttons between different searches (cv search page and vacancies search page) have been removed
    -Behaviour of the "To the hunt" buttons on the Start page has been changed.
    -Fixed the display of some information on the Show CV page (for example, if the end-date of user education is not specified, then the word 'continue' is printed instead).
    
    Co-Authored-By: Kristo Matsumoto <118841278+KristoMatsumoto@users.noreply.github.com>

--- app/controllers/cvs_report.rb
+++ app/controllers/cvs_report.rb
@@ -18,6 +18,7 @@ class CvsReport < Prawn::Document
             cell_style: { padding: [5, 30, 0, 0], borders: [] } 
         ) do | t |
             t.column(0).style(font_style: :bold)
+            t.column(0).text_color = '663C7B'
         end
     end
 
@@ -60,8 +61,8 @@ class CvsReport < Prawn::Document
         @cv.educations.each do | elem | 
             elem_arr = []
             
-            date = "#{elem.start_date.strftime("%d-%m-%Y")} - "
-            elem.end_date.present? ? date += "#{elem.end_date.strftime("%d-%m-%Y")}" : date += "continue" 
+            date = "#{elem.start_date.strftime("%d.%m.%Y")} - "
+            elem.end_date.present? ? date += "#{elem.end_date.strftime("%d.%m.%Y")}" : date += "continue" 
             elem_arr << date
 
             elem_arr << make_table(
@@ -72,8 +73,8 @@ class CvsReport < Prawn::Document
                 ],
                 cell_style: { padding: [0, 0, 0, 0], borders: [] }
             ) do | t |
-                t.row(0).text_color = '663C7B'
-                t.row(0).size = 14
+                t.row(2).text_color = '663C7B'
+                t.row(0).size = 11
                 t.row(0).font_style = :bold
                 t.row(2).font_style = :bold
             end
@@ -88,8 +89,8 @@ class CvsReport < Prawn::Document
         @cv.experiences.each do | elem | 
             elem_arr = []
             
-            date = "#{elem.start_date.strftime("%d-%m-%Y")} - "
-            elem.end_date.present? ? date += "#{elem.end_date.strftime("%d-%m-%Y")}" : date += "continue" 
+            date = "#{elem.start_date.strftime("%d.%m.%Y")} - "
+            elem.end_date.present? ? date += "#{elem.end_date.strftime("%d.%m.%Y")}" : date += "continue" 
             elem_arr << date
 
             elem_arr << make_table(
@@ -97,9 +98,9 @@ class CvsReport < Prawn::Document
                     [elem.position],
                     [elem.company],
                     ["#{elem.country}, #{elem.city}"],
-                    # [elem.outline],
-                    # [elem.responsibilities],
-                    # [elem.achievements]
+                    #[elem.outline],
+                    #[elem.responsibilities],
+                    #[elem.achievements]
                 ],
                 cell_style: { 
                     # overflow: :expand, 
@@ -108,9 +109,10 @@ class CvsReport < Prawn::Document
                     borders: [] 
                 }
             ) do | t |
-                t.row(0).text_color = '663C7B'
+                t.row(1).text_color = '663C7B'
                 t.row(0).size = 14
                 t.row(0).font_style = :bold
+                t.row(1).font_style = :bold
             end
             
             table_arr << elem_arr
@@ -118,7 +120,86 @@ class CvsReport < Prawn::Document
         table_arr
     end
 
+    def languages_data
+        formatted_data = []
+          @cv.languages.map do |elem|
+        
+          arr = 
+          [
+            { text: "#{elem.name} - ", styles: [:bold], color: '262229', size: 11 },
+            { text: elem.level, styles: [:bold], color: 'A6884E', size: 12 },
+          ]
+          if elem.certificates.present?
+            arr << { text: " - #{elem.certificates}", styles: [:bold], color: '262229', size: 11 }
+          end
+          formatted_data << arr
+        end
+        formatted_data
+      end
+    
+    def languages_with_data(data)
+        data.each do |elem|
+            formatted_text elem
+            move_down 2
+        end
+    end
+
+    def consent_statement_line(data)
+        move_down bounds.absolute_bottom
+        text data, size: 9, style: :bold, color: 'D7BE8C'
+        move_down 5
+    end
+
+    def formatted_text_with_data(data)
+        data.each do |elem|
+            formatted_text elem
+            move_down 20
+        end
+        move_up 20
+    end
+
+    def experiences_as_texts
+        formatted_data = @cv.experiences.map do |elem|
+            date = "#{elem.start_date.strftime("%d.%m.%Y")} - "
+            elem.end_date.present? ? date += "#{elem.end_date.strftime("%d.%m.%Y")}" : date += "continue"
+          
+            experience_data = [
+              { text: date, styles: [:normal], color: 'A6884E', size: 12 },
+            ]
+            if elem.company.present?
+                experience_data << { text: elem.company, styles: [:bold], color: '262229', size: 12 }
+            else
+                experience_data << { text: "Freelance/Contract Work", styles: [:bold], color: '262229', size: 12 }
+            end
+            experience_data << { text: elem.position, styles: [:normal], color: 'A343D3', size: 12 }
+            if elem.outline.present?
+                experience_data << { text: "Outline:", styles: [:bold], color: 'A6884E', size: 11 }
+                experience_data << { text: elem.outline, styles: [:normal], color: '262229', size: 10 }
+            end
+            if elem.responsibilities.present?
+                experience_data << { text: "Responsibilities:", styles: [:bold], color: 'A6884E', size: 11 }
+                experience_data << { text: elem.responsibilities, styles: [:normal], color: '262229', size: 10 }
+            end
+            if elem.achievements.present?
+                experience_data << { text: "Achievements:", styles: [:bold], color: 'A6884E', size: 11 }
+                experience_data << { text: elem.achievements, styles: [:normal], color: '262229', size: 10 }
+            end
+            if elem.country.present?
+                if elem.city.present?
+                    experience_data << { text: "#{elem.country}, #{elem.city}", styles: [:bold], color: 'A343D3', size: 11 }
+                else
+                    experience_data << { text: "#{elem.country}", styles: [:bold], color: 'A343D3', size: 11 }
+                end
+            elsif elem.city.present?
+                    experience_data << { text: "#{elem.city}", styles: [:bold], color: 'A343D3', size: 11 }
+            end
+            experience_data.map { |text| text.merge!(text: "#{text[:text]}\n") }
+        end
+    end
+    
+
     def to_pdf(_cv_)
+        font_size 10
         font_families.update(FONTS)
         self.stroke_color = 'A343D3'
         self.line_width = 2
@@ -126,7 +207,21 @@ class CvsReport < Prawn::Document
         @user = User.find_by(id: Person.find_by(id: @cv.person_id).user_id)
         
         font "Inter"
-        text "#{@user.person.name} #{@user.person.surname}", size: 22, style: :bold, color: '663C7B'
+
+        #
+        #bounding_box([0, bounds.height], width: bounds.width*0.3, height: bounds.height) do
+        #    fill_color 'A343D3'
+        #    fill_rectangle [bounds.left, bounds.top], bounds.width, bounds.height
+        #    fill_color '000000'
+        #end
+        #bounding_box([bounds.width*0.3, bounds.height], width: bounds.width*0.7, height: bounds.height) do
+        #    text "This text will flow in a very narrow box starting" +
+        #     "from xxxxx. The pointer will then be moved to [100, 200]" +
+        #     "and return to the margin_box"
+        #end
+        #
+
+        text "#{@user.person.name} #{@user.person.surname}", size: 22, style: :bold
         
         table_for_info(person_info_data)
         if (@user.telephones.present? || @user.emails.present?)
@@ -138,10 +233,12 @@ class CvsReport < Prawn::Document
         stroke_horizontal_rule
 
         if (@cv.about.present? || @cv.skills.present?)
-            move_down 10
+            print_section_title("About me")
             text @cv.about
             
             if (@cv.skills.present?)
+                move_down 10
+                stroke_horizontal_rule
                 print_section_title("Skills")
                 @cv.skills.split("\n").each do | skill |
                     text "- #{skill}"
@@ -160,9 +257,8 @@ class CvsReport < Prawn::Document
             move_down 10
             stroke_horizontal_rule
             print_section_title("Experiences")
-            table_with_data(experiences_data)
+            formatted_text_with_data(experiences_as_texts)
         end
-        text @cv.experiences.inspect
 
         if (@cv.interests.present?)
             move_down 10
@@ -171,15 +267,15 @@ class CvsReport < Prawn::Document
             text @cv.interests
         end
         
-        # move_down 20
-        # stroke_horizontal_rule
-        # move_down 20
+        if (@cv.languages.present?)
+            move_down 10
+            stroke_horizontal_rule
+            print_section_title("Languages")
+            languages_with_data(languages_data)
+        end
 
-        
-        # move_down 20
-        # text @cv.languages.inspect
+        consent_statement_line("*I hereby consent to the processing of my personal information for the purposes of this application.")
 
-        # start_new_page
         render
     end
 end
