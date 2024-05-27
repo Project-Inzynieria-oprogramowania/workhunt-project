363cb7755c5b93498d7acb44cebcd4aef648073f
KristoMatsumoto <118841278+KristoMatsumoto@users.noreply.github.com>
2024-05-27 18:22:10 +0200

    Slider resizing script
    Co-Authored-By: Andrei <162591604+teqoshi@users.noreply.github.com>

--- app/assets/javascripts/slider_resizing.js
+++ app/assets/javascripts/slider_resizing.js
@@ -1,8 +1,24 @@
-var slider = document.getElementById('slider-field');
-var viewportHeight = window.innerHeight;
-slider.style.height = (viewportHeight - 68) + 'px';
-  
-var slideImages = document.querySelectorAll('.slide');
-slideImages.forEach(image => {
-  image.style.height = (viewportHeight - 68) + 'px';
-});
+var slider = document.getElementById('slider-field')
+var header = document.getElementById('header')
+var slider_images = document.querySelectorAll('.slide')
+
+function slider_resize(){
+  var slider_height = `calc(100vh - ${header ? header.offsetHeight : 0}px)`
+
+  slider.style.height = slider_height
+  slider_images.forEach(image => {
+    image.style.transition = 'none'
+    image.style.height = slider_height
+  })
+
+  // Force the re-layout to immediately apply the new dimensions
+  slider.offsetHeight
+
+  slider_images.forEach(image => {
+    image.style.transition = ''
+  })  
+}
+
+slider_resize()
+
+window.addEventListener('resize', slider_resize)
